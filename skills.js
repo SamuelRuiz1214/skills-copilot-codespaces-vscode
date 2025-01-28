9172d1e77e063c3106f448f37eefec591006e74f
SamuelRuiz1214 <157430437+SamuelRuiz1214@users.noreply.github.com>
2025-01-28 20:51:18 +0000

    Copilot first commit

--- skills.js
+++ skills.js
@@ -1,5 +1,3 @@
-function CalculateNumbers(var1, var2) {
-    return var1 + var2;
+function calculateNumbers(var1, var2) {
+  return var1 + var2;
 }
-
-console.log(CalculateNumbers(1, 2)); // 3       
