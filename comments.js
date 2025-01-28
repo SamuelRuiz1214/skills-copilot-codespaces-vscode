a99faeb5118d9d2fe2b96cf17b7098a1f5e954ef
SamuelRuiz1214 <157430437+SamuelRuiz1214@users.noreply.github.com>
2025-01-28 22:18:34 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,84 +1,17 @@
-// create web server
+//create web server
+const express = require('express'); //import express
+const app = express(); //create an express app
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+const commentsPath = path.join(__dirname, 'comments.json');
+const comments = require(commentsPath);
 
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-var template = require('./lib/template.js');
-var path = require('path');
-var sanitizeHtml = require('sanitize-html');
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
-var app = http.createServer(function(request, response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;
-    var description = queryData.id;
-    var list = template.list();
-    var html = template.HTML(title, list,
-        `<h2>${title}</h2>${description}`,
-        `<a href="/create">create</a>`
-    );
+app.get('/comments', (req, res) => {
+    res.json(comments);
+}
+);
 
-    if(pathname === '/'){
-        if(queryData.id === undefined){
-            fs.readdir('./data', function(error, filelist){
-                title = 'Welcome';
-                description = 'Hello, Node.js';
-                var list = template.list(filelist);
-                var html = template.HTML(title, list,
-                    `<h2>${title}</h2>${description}`,
-                    `<a href="/create">create</a>`
-                );
-                response.writeHead(200);
-                response.end(html);
-            });
-        } else {
-            fs.readdir('./data', function(error, filelist){
-                var filteredId = path.parse(queryData.id).base;
-                fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-                    var title = queryData.id;
-                    var sanitizedTitle = sanitizeHtml(title);
-                    var sanitizedDescription = sanitizeHtml(description, {
-                        allowedTags: ['h1']
-                    });
-                    var list = template.list(filelist);
-                    var html = template.HTML(sanitizedTitle, list,
-                        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-                        `<a href="/create">create</a>
-                        <a href="/update?id=${sanitizedTitle}">update</a>
-                        <form action="delete_process" method="post">
-                            <input type="hidden" name="id" value="${sanitizedTitle}">
-                            <input type="submit" value="delete">
-                        </form>`
-                    );
-                    response.writeHead(200);
-                    response.end(html);
-                });
-            });
-        }
-    } else if(pathname === '/create'){
-        fs.readdir('./data', function(error, filelist){
-            title = 'WEB - create';
-            var list = template.list(filelist);
-            var html = template.HTML(title, list, `
-                <form action="/create_process" method="post">
-                    <p><input type="text" name="title" placeholder="title"></p>
-                    <p>
-                                    <textarea name="description" placeholder="description"></textarea>
-                                </p>
-                                <p>
-                                    <input type="submit">
-                                </p>
-                            </form>
-                                                `);
-                                    response.writeHead(200);
-                                    response.end(html);
-                                });
-                            } else if(pathname === '/update'){
-                                // Add your update logic here
-                            } else {
-                                response.writeHead(404);
-                                response.end('Not found');
-                            }
-                        });
